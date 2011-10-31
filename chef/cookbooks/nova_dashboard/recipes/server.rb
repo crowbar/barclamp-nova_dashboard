a5061b1f1e1a4cdf03992049a242d480d4ee0c31
Justin Shepherd <jshepher@rackspace.com>
2011-10-31 14:47:49 -0500

    using remote keystone and mysql services

--- chef/cookbooks/nova_dashboard/recipes/server.rb
+++ chef/cookbooks/nova_dashboard/recipes/server.rb
@@ -16,6 +16,7 @@
 include_recipe "apache2"
 include_recipe "apache2::mod_wsgi"
 include_recipe "apache2::mod_rewrite"
+include_recipe "mysql::client"
 
 packages = [ "openstack-dashboard", "django-openstack", "openstackx", "python-django", "python-mysqldb" ]
 packages.each do |pkg|
@@ -47,51 +48,6 @@ end
 #  action :create
 #end
 
-mysql_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(mysql, "admin").address if mysql_address.nil?
-Chef::Log.info("Mysql server found at #{mysql_address}")
-
-keystone_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(keystone, "admin").address if keystone_address.nil?
-Chef::Log.info("Rabbit server found at #{rabbit_address}")
-
-# Need to template the "EXTERNAL_MONITORING" array
-template "/var/lib/dash/local/local_settings.py" do
-  source "local_settings.py.erb"
-  variables(
-    :user => node[:dash][:db_user],
-    :passwd => node[:dash][:db_passwd],
-    :keystone_address => keystone_address,
-    :mysql_address => mysql_address,
-    :ip_address => node[:controller_ipaddress],
-    :db_name => node[:dash][:db]
-  )
-  action :create
-#  owner  "www-data"
-end
-
-execute "PYTHONPATH=/var/lib/dash/ python dashboard/manage.py syncdb" do
-  cwd "/var/lib/dash"
-  environment ({'PYTHONPATH' => '/var/lib/dash/'})
-  command "python dashboard/manage.py syncdb"
-  action :run
-  notifies :restart, resources(:service => "apache2"), :immediately
-end
-
-#bash "dash-db" do
-#  code <<-EOH
-#PATH="/usr/bin:/bin"
-#python /var/lib/dash/dashboard/manage.py syncdb
-#EOH
-#  user "www-data"
-#  action :nothing
-#end
-
-#file "/var/lib/dash/local/dashboard_openstack.sqlite3" do
-#  owner "www-data"
-#  mode "0600"
-#  action :create
-#  notifies :run, "bash[dash-db]"
-#end
-
 apache_site "000-default" do
   enable false
 end
@@ -100,12 +56,68 @@ apache_site "dash" do
   enable true
 end
 
-#web_app "nova_dashboard" do
-#  server_name "nova_dashboard"
-#  docroot "/var/lib/dash/.blackhole"
-#  template "web_app.conf.erb"
-#  web_port 80
-#end
+mysqls = search(:node, "recipes:mysql\\:\\:server") || []
+if mysqls.length > 0
+  mysql = mysqls[0]
+else
+  mysql = node
+end
+
+mysql_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(mysql, "admin").address if mysql_address.nil?
+Chef::Log.info("Mysql server found at #{mysql_address}")
+
+# Create the Dashboard Database
+mysql_database "create #{node[:dashboard][:db][:database]} database" do
+  host	mysql_address
+  username "db_maker"
+  password mysql[:mysql][:db_maker_password]
+  database node[:dashboard][:db][:database]
+  action :create_db
+end
+
+mysql_database "create dashboard database user" do
+  host	mysql_address
+  username "db_maker"
+  password mysql[:mysql][:db_maker_password]
+  database node[:dashboard][:db][:database]
+  action :query
+  sql "GRANT ALL on #{node[:dashboard][:db][:database]}.* to '#{node[:dashboard][:db][:user]}'@'%' IDENTIFIED BY '#{node[:dashboard][:db][:password]}';"
+end
+
+# Need to figure out environment filter
+keystones = search(:node, "recipes:keystone\\:\\:server") || []
+if keystones.length > 0
+  keystone = keystones[0]
+else
+  keystone = node
+end
+
+keystone_address = keystone[:keystone][:address]
+keystone_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(keystone, "admin").address if keystone_address.nil?
+Chef::Log.info("Keystone server found at #{keystone_address}")
+
+# Need to template the "EXTERNAL_MONITORING" array
+template "/var/lib/dash/local/local_settings.py" do
+  source "local_settings.py.erb"
+  variables(
+    :keystone_admin_token => keystone[:keystone][:dashboard][:long-lived-token],
+    :keystone_address => keystone_address,
+    :mysql_address => mysql_address,
+    :mysql_db_name => node[:dashboard][:db][:database],
+    :mysql_user => node[:dashboard][:db][:user],
+    :mysql_passwd => node[:dashboard][:db][:passwd]
+  )
+  notifies :run, resources(:execute => "dashboard/manage.py syncdb"), :immediately
+  action :create
+end
+
+execute "dashboard/manage.py syncdb" do
+  cwd "/var/lib/dash"
+  environment ({'PYTHONPATH' => '/var/lib/dash/'})
+  command "python dashboard/manage.py syncdb"
+  action :nothing
+  notifies :restart, resources(:service => "apache2"), :immediately
+end
 
 node[:nova_dashboard][:monitor][:svcs] <<["nova_dashboard-server"]
 node.save
