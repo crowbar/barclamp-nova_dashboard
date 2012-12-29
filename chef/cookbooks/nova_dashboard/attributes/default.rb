ba77019417d4e20b54b47376818aacff431589ab
Greg Althaus <galthaus@austin.rr.com>
2012-12-29 11:43:05 -0600

    Fix chefspec warnings.

--- chef/cookbooks/nova_dashboard/attributes/default.rb
+++ chef/cookbooks/nova_dashboard/attributes/default.rb
@@ -21,7 +21,7 @@ override[:nova_dashboard][:user]="nova_dashboard"
 default[:nova_dashboard][:site_branding] = "Openstack Nova Dashboard"
 
 # declare what needs to be monitored
-node[:nova_dashboard][:monitor]={}
-node[:nova_dashboard][:monitor][:svcs] = []
-node[:nova_dashboard][:monitor][:ports]={}
+node.set[:nova_dashboard][:monitor]={}
+node.set[:nova_dashboard][:monitor][:svcs] = []
+node.set[:nova_dashboard][:monitor][:ports]={}
 
