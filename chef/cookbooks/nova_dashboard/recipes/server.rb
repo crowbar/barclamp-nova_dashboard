8b6aec27dfd36bc49ba2eae8671fdcda6d811e91
Vincent Untz <vuntz@suse.com>
2013-07-02 11:50:00 +0200

    Set OPENSTACK_SSL_NO_VERIFY if keystone certificate is not secure

--- chef/cookbooks/nova_dashboard/recipes/server.rb
+++ chef/cookbooks/nova_dashboard/recipes/server.rb
@@ -220,6 +220,7 @@ keystone_address = keystone["keystone"]["address"] rescue nil
 keystone_address = Chef::Recipe::Barclamp::Inventory.get_network_by_type(keystone, "admin").address if keystone_address.nil?
 keystone_protocol = keystone["keystone"]["api"]["protocol"]
 keystone_service_port = keystone["keystone"]["api"]["service_port"] rescue nil
+keystone_insecure = keystone_protocol == 'https' && keystone[:keystone][:ssl][:insecure]
 Chef::Log.info("Keystone server found at #{keystone_address}")
 
 execute "python manage.py syncdb" do
@@ -241,6 +242,7 @@ template "#{dashboard_path}/openstack_dashboard/local/local_settings.py" do
     :keystone_protocol => keystone_protocol,
     :keystone_address => keystone_address,
     :keystone_service_port => keystone_service_port,
+    :insecure => keystone_insecure,
     :db_settings => db_settings,
     :compress_offline => node.platform == "suse"
   )
