334a3e986681797ee556e541c40184e3cefd2e2d
Vincent Untz <vuntz@suse.com>
2014-01-15 09:24:20 +0100

    Fix typo breaking recipe

--- chef/cookbooks/nova_dashboard/recipes/server.rb
+++ chef/cookbooks/nova_dashboard/recipes/server.rb
@@ -319,7 +319,7 @@ template "#{dashboard_path}/openstack_dashboard/local/local_settings.py" do
     :timezone => (node[:provisioner][:timezone] rescue "UTC") || "UTC",
     :use_ssl => node[:nova_dashboard][:apache][:ssl],
     :site_branding => node[:nova_dashboard][:site_branding],
-    :neutron_networking_plugin = neutron_networking_plugin,
+    :neutron_networking_plugin => neutron_networking_plugin,
     :session_timeout => node[:nova_dashboard][:session_timeout]
   )
   notifies :run, resources(:execute => "python manage.py syncdb"), :immediately
