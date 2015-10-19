crowbar/crowbar-openstack

increase timeout for synchronization of horizon HA resources (bsc#935462)
Horizon HA resources can take over 1 minute to be set up, as
seen by mkcloud failures in ci.suse.de Jenkins.

chef/cookbooks/horizon/recipes/ha.rb
@@ -37,7 +37,9 @@ end
 crowbar_pacemaker_sync_mark "sync-horizon_before_ha"
 
 # Avoid races when creating pacemaker resources
-crowbar_pacemaker_sync_mark "wait-horizon_ha_resources"
+crowbar_pacemaker_sync_mark "wait-horizon_ha_resources" do
+  timeout 180
+end
 
 include_recipe "crowbar-pacemaker::apache"
 

crowbar/barclamp-nova_dashboard

Cherry-picked from f8ef6a007d1d67fc390b8c305cc9d012f623c8e7 (crowbar-openstack)
(https://bugzilla.suse.com/show_bug.cgi?id=935462)

chef/cookbooks/nova_dashboard/recipes/ha.rb
@@ -37,7 +37,9 @@ end
 crowbar_pacemaker_sync_mark "sync-nova_dashboard_before_ha"
 
 # Avoid races when creating pacemaker resources
-crowbar_pacemaker_sync_mark "wait-nova_dashboard_ha_resources"
+crowbar_pacemaker_sync_mark "wait-nova_dashboard_ha_resources" do
+  timeout 180
+end
 
 include_recipe "crowbar-pacemaker::apache"
 